94872b1713e0db979d5ee5fc07443dcabc10aa0e
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-14 12:46:50 +0530

    [Prashant]: find maximum integer number

---
+++ FindMaxNumberProgram/FindMaxiumNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxNumberProgram
+{
+    public class FindMaxiumNumber
+    {
+        public static int MaximumIntergerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstnumber,secondnumber,thirdnumber are same");
+        }
+    }
+        
+
+
+    
+}
---
+++ FindMaxNumberProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FindMaxNumberProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            int output = FindMaxiumNumber.MaximumIntergerNumber(11, 22, 33);
+            Console.WriteLine(output);
+
+        }
+    }
+}
