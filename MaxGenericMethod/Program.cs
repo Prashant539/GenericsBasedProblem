50f1b1107b5b445381d51530dd74b24f192e3a53
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-14 19:25:20 +0530

    [Prashant]:Print max value using generic

---
+++ MaxGenericMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MaxGenericMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 112, 123, 234, 345 };
+            GenericMaxNumber<int> generic = new GenericMaxNumber<int>(intArray);
+            generic.PrintMaxValue();
+
+            double[] doubleArray = { 112.33, 123.53, 234.43, 345.63 };
+            GenericMaxNumber<double> genericDouble = new GenericMaxNumber<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+
+            string[] stringArray = { "234", "673", "359", "872" };
+            GenericMaxNumber<string> genericString = new GenericMaxNumber<string>(stringArray);
+            genericString.PrintMaxValue();
+        }
+    }
+}
