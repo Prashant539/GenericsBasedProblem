31305a3875b04922cd3b7c42b363b2f83e76c342
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-14 12:50:52 +0530

    [Prashant]: find maximum string

---
+++ MaximumStringProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MaximumStringProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max String");
+            string output =MaxString.MaximumString("apple", "banana", "prashant" );
+            Console.WriteLine(output);
+        }
+    }
+}
