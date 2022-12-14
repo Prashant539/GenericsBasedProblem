53167164068644b27bf978ff5be721f6143a98f8
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-14 12:43:33 +0530

    [Prashant]:Print Array using Generic

---
+++ GenericsSampleCode/Generic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsSampleCode
+{
+    internal class Generic
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var item in inputArray)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("...........");
+        }
+    }
+}
---
+++ GenericsSampleCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using GenericsSampleCode;
+using System;
+namespace GenericsBasedCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, };
+
+            double[] doubleArray = { 1.1, 2.2, 3.3 };
+            char[] charArray = { 'a', 'b', 'c', 'd' };
+            Generic.toPrint<int>(intArray);
+            Generic.toPrint<double>(doubleArray);
+            Generic.toPrint<char>(charArray);
+        }
+    }
+}
+
+
+
+
+
+
+
