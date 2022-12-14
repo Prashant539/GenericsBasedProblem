138a686b7d8713089d0c93a6bc8d9f1112f76007
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-14 12:41:32 +0530

    [Prashant]:Print array using console

---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            SampleCode.toPrint(intArray);
+            SampleCode.toPrint(charArray);
+            SampleCode.toPrint(doubleArray);
+        }
+    }
+}
+
+
